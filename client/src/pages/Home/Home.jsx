ad8a25c7a74d828d04830a587cd4bd4537a3af7d
Samolazoff <samolazov.roman.alex@gmail.com>
2024-11-23 17:58:45 +0300

    Layout Home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,26 @@
 import './Home.scss';
 
+import { Link } from 'react-router-dom';
+
 function Home() {
     return(
-        <h1>Home</h1>
+       <div className="app">
+            <heaeder className="app-header container">
+                <Link to='/'>
+                    <h1 className="title">
+                        Paralect test
+                    </h1>
+                </Link>
+                <div className="box-btns">
+                    <button>Add</button>
+                    <button>Delete</button>
+                </div>
+            </heaeder>
+            <main className="app-main">
+                main
+            </main>
+            <footer className="app-footer">footer</footer>
+       </div>
     )
 };
 
