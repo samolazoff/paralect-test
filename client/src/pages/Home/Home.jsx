fad70afed1b428ab1249075e0a2c006064feb611
Samolazoff <samolazov.roman.alex@gmail.com>
2024-11-24 14:28:48 +0300

    Add table

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -16,10 +16,38 @@ function Home() {
                     <button className='btn-main btn__delete'>Delete</button>
                 </div>
             </heaeder>
-            <main className="app-main">
-                main
+            <main className="app-main container">
+                <h2 className='title-block'>Responses</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th scope="col">Company</th>
+                            <th scope="col">Vacancy</th>
+                            <th scope="col">Salary range</th>
+                            <th scope="col">Response status</th>
+                            <th scope="col">Note</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Alfa Qualetet</th>
+                            <td>Web </td>
+                            <td>2000-3000</td>
+                            <td>Hidden</td>
+                            <td>Dodo</td>
+                            <td>
+                                <div className="btn-table">
+                                    <button type="submit" className='btn__delete'>del</button>
+                                    <button type="submit">red</button>
+                                </div>
+                            </td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
             </main>
-            <footer className="app-footer">footer</footer>
+            <footer className="app-footer container">footer</footer>
        </div>
     )
 };
